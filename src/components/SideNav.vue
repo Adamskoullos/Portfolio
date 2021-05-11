751686d87bd9571dc42f97ad5be8fa3ce236dc56
Adam <adamskoullos@gmail.com>
2021-05-11 11:20:37 +0100

    Add resume and deploy to firebase

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,319 +1,316 @@
 <template>
-          <nav >
-            <div class="logo" >
-                <transition appear
-                @before-enter="imageBeforeEnter"
-                @enter="imageEnter">
-                    <div class="image" @click="handleLogout">
-                        <img src="../assets/images/profile_pic.jpg" alt="Adam Skoullos">
-                    </div>
-                </transition>
-                <div>{{ error }}</div>
-                <transition appear
-                @before-enter="nameBeforeEnter"
-                @enter="nameEnter">
-                <div @click="handleLogin">
-                    <h3 >Adam Skoullos</h3>
-                    <h5 v-if="user" class="logged-in">Developer</h5>
-                    <h5 v-if="!user">Developer</h5>
-                </div>
-                </transition>
-            </div>
-            <transition appear
-            @before-enter="ulBeforeEnter"
-            @enter="ulEnter">
-            <ul class="navbar-nav">
-                <li class="nat-item" v-if="user">
-                    <router-link :to="{ name: 'NewBlog' }" @click="handleScroll" v-if="user">
-                        <span class="material-icons">create</span>
-                        <h5>New post</h5>
-                    </router-link>
-                </li>
-                <li class="nav-item" >
-                    <router-link :to="{ name: 'Home' }" @click="handleScroll">
-                        <i class="bi bi-house-fill"></i>
-                        <h5>Home</h5>
-                    </router-link>
-                </li>
-                <li class="nav-item" >
-                    <router-link :to="{ name: 'Projects' }" @click="handleScroll">
-                        <i class="bi bi-grid-fill"></i>
-                        <h5>Projects</h5>
-                    </router-link>
-                </li>
-                <li class="nav-item" >
-                    <router-link :to="{ name: 'Blog' }" @click="handleScroll">
-                        <i class="bi bi-chat-left-text-fill"></i>
-                        <h5>Blog</h5>
-                    </router-link>
-                </li>
-                <li class="nav-item" >
-                    <router-link :to="{ name: 'Certifications' }" @click="handleScroll">
-                        <i class="bi bi-award-fill"></i>
-                        <h5>Certifications</h5>
-                    </router-link>
-                </li>
-                <li class="nav-item" >
-                    <router-link :to="{ name: 'Contact' }">
-                        <i class="bi bi-envelope-fill"></i>
-                        <h5>Contact</h5>
-                    </router-link>
-                </li>
-                <li class="nav-item" >
-                    <a href="https://github.com/Adamskoullos" target="_blank">
-                        <i class="bi bi-github"></i>
-                        <h5>GitHub Profile</h5>
-                    </a>
-                </li>
-                <li class="nav-item" >
-                    <a href="https://drive.google.com/file/d/1IwWi7S0WE8t9c4LKK0aTs_Y3b0YrDq8x/view?usp=sharing" download target="_blank">
-                        <i class="bi bi-file-earmark-person-fill"></i>
-                        <h5>Resume PDF</h5>
-                    </a>
-                </li>
-            </ul>
-            </transition>
-          </nav>
+  <nav>
+    <div class="logo">
+      <transition appear @before-enter="imageBeforeEnter" @enter="imageEnter">
+        <div class="image" @click="handleLogout">
+          <img src="../assets/images/profile_pic.jpg" alt="Adam Skoullos" />
+        </div>
+      </transition>
+      <div>{{ error }}</div>
+      <transition appear @before-enter="nameBeforeEnter" @enter="nameEnter">
+        <div @click="handleLogin">
+          <h3>Adam Skoullos</h3>
+          <h5 v-if="user" class="logged-in">Developer</h5>
+          <h5 v-if="!user">Developer</h5>
+        </div>
+      </transition>
+    </div>
+    <transition appear @before-enter="ulBeforeEnter" @enter="ulEnter">
+      <ul class="navbar-nav">
+        <li class="nat-item" v-if="user">
+          <router-link
+            :to="{ name: 'NewBlog' }"
+            @click="handleScroll"
+            v-if="user"
+          >
+            <span class="material-icons">create</span>
+            <h5>New post</h5>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Home' }" @click="handleScroll">
+            <i class="bi bi-house-fill"></i>
+            <h5>Home</h5>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Projects' }" @click="handleScroll">
+            <i class="bi bi-grid-fill"></i>
+            <h5>Projects</h5>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Blog' }" @click="handleScroll">
+            <i class="bi bi-chat-left-text-fill"></i>
+            <h5>Blog</h5>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Certifications' }" @click="handleScroll">
+            <i class="bi bi-award-fill"></i>
+            <h5>Certifications</h5>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Contact' }">
+            <i class="bi bi-envelope-fill"></i>
+            <h5>Contact</h5>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a href="https://github.com/Adamskoullos" target="_blank">
+            <i class="bi bi-github"></i>
+            <h5>GitHub Profile</h5>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            href="https://drive.google.com/file/d/1hsHfdyLpC0RpHjTWLLpLD3MnUdTKZiq7/view?usp=sharing"
+            download
+            target="_blank"
+          >
+            <i class="bi bi-file-earmark-person-fill"></i>
+            <h5>Resume PDF</h5>
+          </a>
+        </li>
+      </ul>
+    </transition>
+  </nav>
 </template>
 
 <script>
-import gsap from 'gsap'
-import useLogout from '../composables/useLogout'
-import { useRouter } from 'vue-router'
-import { ref } from '@vue/reactivity'
+  import gsap from "gsap";
+  import useLogout from "../composables/useLogout";
+  import { useRouter } from "vue-router";
+  import { ref } from "@vue/reactivity";
 
-export default {
-    props: ['login', 'user'],
-    setup(props, context){
-        
-        const { logout, error } = useLogout()
-        const router = useRouter()
-        
-        const handleScroll = ()=>{
-            context.emit('scroll')
-        }
+  export default {
+    props: ["login", "user"],
+    setup(props, context) {
+      const { logout, error } = useLogout();
+      const router = useRouter();
 
-        const imageBeforeEnter = (el) => {
-          el.style.transform = 'scale(0)'
-          el.style.opacity = 1
-        }
+      const handleScroll = () => {
+        context.emit("scroll");
+      };
 
-        const imageEnter = (el) => {
-          gsap.to(el, {
-            x:0,
-            opacity: 1,
-            scale: 1,
-            delay:1.9,
-            duration: 1,
-            ease: 'elastic',
-          })
-        }
+      const imageBeforeEnter = (el) => {
+        el.style.transform = "scale(0)";
+        el.style.opacity = 1;
+      };
 
-        const nameBeforeEnter = (el) => {
-          el.style.transform = 'scale(0)'
-          el.style.opacity = 1
-        }
+      const imageEnter = (el) => {
+        gsap.to(el, {
+          x: 0,
+          opacity: 1,
+          scale: 1,
+          delay: 1.9,
+          duration: 1,
+          ease: "elastic",
+        });
+      };
 
-        const nameEnter = (el) => {
-          gsap.to(el, {
-            x:0,
-            opacity: 1,
-            scale: 1,
-            delay:1.2,
-            duration: 1,
-            ease: 'elastic',
-          })
-        }
+      const nameBeforeEnter = (el) => {
+        el.style.transform = "scale(0)";
+        el.style.opacity = 1;
+      };
 
-        const ulBeforeEnter = (el) => {
+      const nameEnter = (el) => {
+        gsap.to(el, {
+          x: 0,
+          opacity: 1,
+          scale: 1,
+          delay: 1.2,
+          duration: 1,
+          ease: "elastic",
+        });
+      };
+
+      const ulBeforeEnter = (el) => {
         //   el.style.transform = 'translateX(-400px)'
-            el.style.transform = 'scale(0)'
-            el.style.opacity = 1
-        }
+        el.style.transform = "scale(0)";
+        el.style.opacity = 1;
+      };
 
-        const ulEnter = (el) => {
-          gsap.to(el, {
-            x:0,
-            opacity: 1,
-            scale: 1,
-            delay:0.7,
-            duration: 0.2,
-            ease: 'sine',
-          })
-        }
+      const ulEnter = (el) => {
+        gsap.to(el, {
+          x: 0,
+          opacity: 1,
+          scale: 1,
+          delay: 0.7,
+          duration: 0.2,
+          ease: "sine",
+        });
+      };
 
-        const handleLogout = async () => {
-            await logout()
-            router.push({ name: 'Home' })
-        }
+      const handleLogout = async () => {
+        await logout();
+        router.push({ name: "Home" });
+      };
 
-        const handleLogin = () => {
-            router.push({ name: 'Login' })
-        }
+      const handleLogin = () => {
+        router.push({ name: "Login" });
+      };
 
-        return {
-            handleScroll, 
-            imageBeforeEnter, 
-            imageEnter, 
-            nameBeforeEnter, 
-            nameEnter,
-            ulBeforeEnter,
-            ulEnter,
-            handleLogout,
-            handleLogin,
-            error,}
-    }
-}
+      return {
+        handleScroll,
+        imageBeforeEnter,
+        imageEnter,
+        nameBeforeEnter,
+        nameEnter,
+        ulBeforeEnter,
+        ulEnter,
+        handleLogout,
+        handleLogin,
+        error,
+      };
+    },
+  };
 </script>
 
 <style lang="scss">
+  nav::-webkit-scrollbar {
+    width: 0px; /* width of the entire scrollbar */
+  }
 
-nav::-webkit-scrollbar {
-    width: 0px;               /* width of the entire scrollbar */
-}
+  nav::-webkit-scrollbar-track {
+    background: rgb(168, 168, 168); /* color of the tracking area */
+  }
 
-nav::-webkit-scrollbar-track {
-background: rgb(168, 168, 168);        /* color of the tracking area */
-}
-
-nav{
+  nav {
     height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: stretch;
-    justify-content:flex-start;
+    justify-content: flex-start;
     position: relative;
     z-index: 10;
     min-width: 230px;
     box-shadow: 1px 0px 1px rgba(90, 90, 90, 0.1);
     overflow: auto;
 
-    
-    
     .logo {
-        margin: 5vh auto;
+      margin: 5vh auto;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100;
+
+      .image {
         display: flex;
-        flex-direction: column;
         align-items: center;
         justify-content: center;
-        width: 100;
-        
-        .image{
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 150px;
-            width: 150px;
-            overflow: hidden;
-            border-radius: 50%;
-            // box-shadow: 0px 0px 6px rgba(50,50,50,0.4);
-            box-shadow: 1px 1px 4px rgba(50,50,50,0.4);
-            img{
-                flex:1;
-                margin-left: auto;
-                margin-right: auto;
-                width: 100%;
-            }
+        height: 150px;
+        width: 150px;
+        overflow: hidden;
+        border-radius: 50%;
+        // box-shadow: 0px 0px 6px rgba(50,50,50,0.4);
+        box-shadow: 1px 1px 4px rgba(50, 50, 50, 0.4);
+        img {
+          flex: 1;
+          margin-left: auto;
+          margin-right: auto;
+          width: 100%;
         }
-        i{
-            font-size: 30px;
-        }
-        i::before{
-            vertical-align: -.2em;
-        }
-        div {
+      }
+      i {
+        font-size: 30px;
+      }
+      i::before {
+        vertical-align: -0.2em;
+      }
+      div {
         padding-top: 10px;
-        
-            h3{
-            font-weight: 200;
-            }
-            h5{
-                font-weight: 300;
-            }
-            .logged-in{
-                color: $green;
-                font-weight: 500;
-            }
+
+        h3 {
+          font-weight: 200;
         }
+        h5 {
+          font-weight: 300;
+        }
+        .logged-in {
+          color: $green;
+          font-weight: 500;
+        }
+      }
     }
-    ul{
+    ul {
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+      li {
+        // background: blueviolet;
+        display: flex;
+        justify-content: stretch;
+        a {
+          // background: cadetblue;
+          width: 100%;
+          display: flex;
+          align-items: center;
+          text-decoration: none;
+          height: 60px;
+          margin-right: 3px;
+          background: white;
+          transition: all ease 0s;
+          h5 {
+            display: inline-block;
+            margin: auto 10px;
+            font-weight: 300;
+          }
+          i {
+            font-size: 22px;
+            margin-left: 15px;
+            color: $icon;
+          }
+          span {
+            font-size: 22px;
+            margin-left: 15px;
+            color: $icon;
+          }
+        }
+        a:hover {
+          transform: scale(1.02);
+          box-shadow: $box-shadow;
+          transition: all ease 0.1s;
+        }
+        a.projects {
+          background: rgba(255, 255, 255, 0);
+        }
+        a.projects:hover {
+          transform: scale(1);
+          box-shadow: 0 0 0 rgb(0, 0, 0, 0);
+        }
+        a.project {
+          width: 100%;
+          background: olive;
+        }
+        .router-link-active {
+          transition: all ease 0.1s;
+          transform: scale(1.02);
+          box-shadow: $box-shadow;
+          h5 {
+            color: $active-link-font-color;
+            font-weight: 300;
+          }
+          i,
+          span {
+            color: $active-link-icon;
+          }
+        }
+        .nav-link {
+          display: flex;
+          padding: 0;
+        }
+      }
+      .drop-down-ul {
+        width: 100%;
+      }
+      .drop-down {
         display: flex;
         flex-direction: column;
-        align-items: stretch;
-        li{
-            // background: blueviolet;
-            display: flex;
-            justify-content: stretch;
-            a{
-                // background: cadetblue;
-                width: 100%;
-                display: flex;
-                align-items: center;
-                text-decoration: none;
-                height: 60px;
-                margin-right: 3px;
-                background: white;
-                transition: all ease 0s;
-                h5{
-                    display: inline-block;
-                    margin: auto 10px;
-                    font-weight: 300;
-                }
-                i{
-                    font-size: 22px;
-                    margin-left: 15px;
-                    color: $icon;
-                }
-                span{
-                    font-size: 22px;
-                    margin-left: 15px;
-                    color: $icon;
-                }
-            }
-            a:hover{
-                transform: scale(1.02);
-                box-shadow: $box-shadow;
-                transition: all ease 0.1s;
-            }
-            a.projects{
-                background: rgba(255, 255, 255, 0);;
-            }
-            a.projects:hover{
-                transform: scale(1);
-                box-shadow: 0 0 0 rgb(0,0,0,0);
-            }
-            a.project{
-                width: 100%;
-                background: olive;
-            }
-            .router-link-active{
-                transition: all ease 0.1s;
-                transform: scale(1.02);
-                box-shadow: $box-shadow;
-                h5{
-                    color:$active-link-font-color;
-                    font-weight: 300;
-                }
-                i, span{
-                    color: $active-link-icon;
-                }
-            }
-            .nav-link{
-                display: flex;
-                padding: 0;
-
-                
-            }
-        }
-        .drop-down-ul{
-            width: 100%;
-        }
-        .drop-down{
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-
-        }
+        align-items: flex-start;
+      }
     }
-}
+  }
 </style>
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,132 +1,131 @@
 <template>
-    <div class="top-nav-container row">
-        <div class="logo col-12 col-sm-4" >
-            <router-link :to="{ name: 'Projects' }" @click="handleScroll">
-                <h2 >Adam Skoullos</h2>
-            </router-link>
-        </div>
-        <nav class="top-nav-row col-12 col-sm-8">
-            <ul class="">
-                <li class="nav-item" >
-                    <router-link :to="{ name: 'Home' }" @click="handleScroll">
-                        <i class="bi bi-house-fill"></i>
-                    </router-link>
-                </li>
-                <li class="nav-item" >
-                    <router-link :to="{ name: 'Projects' }" @click="handleScroll">
-                        <i class="bi bi-grid-fill"></i>
-                    </router-link>
-                </li>
-                <li class="nav-item" >
-                  <router-link :to="{ name: 'Blog' }" @click="handleScroll">
-                      <i class="bi bi-chat-left-text-fill"></i>
-                  </router-link>
-                </li>
-                <li class="nav-item" >
-                    <a href="https://github.com/Adamskoullos" target="_blank">
-                        <i class="bi bi-github"></i>
-                    </a>
-                </li>
-                <li class="nav-item" >
-                    <a href="https://drive.google.com/file/d/1IwWi7S0WE8t9c4LKK0aTs_Y3b0YrDq8x/view?usp=sharing" download target="_blank">
-                        <i class="bi bi-file-earmark-person-fill"></i>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+  <div class="top-nav-container row">
+    <div class="logo col-12 col-sm-4">
+      <router-link :to="{ name: 'Projects' }" @click="handleScroll">
+        <h2>Adam Skoullos</h2>
+      </router-link>
     </div>
+    <nav class="top-nav-row col-12 col-sm-8">
+      <ul class="">
+        <li class="nav-item">
+          <router-link :to="{ name: 'Home' }" @click="handleScroll">
+            <i class="bi bi-house-fill"></i>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Projects' }" @click="handleScroll">
+            <i class="bi bi-grid-fill"></i>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Blog' }" @click="handleScroll">
+            <i class="bi bi-chat-left-text-fill"></i>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a href="https://github.com/Adamskoullos" target="_blank">
+            <i class="bi bi-github"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            href="https://drive.google.com/file/d/1hsHfdyLpC0RpHjTWLLpLD3MnUdTKZiq7/view?usp=sharing"
+            download
+            target="_blank"
+          >
+            <i class="bi bi-file-earmark-person-fill"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
-export default {
-    setup(props, context){
-        const handleScroll = ()=>{
-            context.emit('scroll')
-        }
+  export default {
+    setup(props, context) {
+      const handleScroll = () => {
+        context.emit("scroll");
+      };
 
-        return {handleScroll}
-    }
-    
-}
+      return { handleScroll };
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.top-nav-container{
+  .top-nav-container {
     display: flex;
     align-items: center;
     // justify-content: ;
     background: rgb(255, 255, 255);
-    flex:1;
-    box-shadow: 0px 1px 5px rgba(50,50,50,0.3);
+    flex: 1;
+    box-shadow: 0px 1px 5px rgba(50, 50, 50, 0.3);
     z-index: 9;
 
-    .top-nav-row{
-    height: 100%;
-    flex:1;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    box-shadow: 0 0 0;
-    margin: auto 15px;
-    background: white;
-    z-index: 10;
+    .top-nav-row {
+      height: 100%;
+      flex: 1;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      box-shadow: 0 0 0;
+      margin: auto 15px;
+      background: white;
+      z-index: 10;
 
-        ul{
-            margin: auto 0;
+      ul {
+        margin: auto 0;
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        // background: red;
+        padding-left: 0;
+        padding-right: 0;
+        margin-right: 15px;
+
+        li {
+          a {
+            box-shadow: 1px 1px 5px rgba(50, 50, 50, 0);
+            height: 100%;
             display: flex;
-            flex-wrap: wrap;
-            flex-direction: row;
             align-items: center;
             justify-content: center;
-            // background: red;
-            padding-left: 0;
-            padding-right: 0;
-            margin-right: 15px;
-
-            li{
-                a{
-                box-shadow: 1px 1px 5px rgba(50,50,50,0);
-                height: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                margin: 0;
-                    i{
-                    font-size: 26px;
-                    margin: auto 10px;
-                    color: $icon;
-                    padding-top: 5px;
-                    }
-                    
-                    i:hover{
-                        transform: scale(1.1);
-                    }
-                }
-                
-                .router-link-active{
-                    box-shadow: 1px 1px 5px rgba(50,50,50,0);
-                    
-                    i{
-                        color: $active-link-icon;
-                        transform: scale(1.2);
-                        transition: all ease 0.1s;
-                        padding-top: 4px;
-                    }
-                }
+            margin: 0;
+            i {
+              font-size: 26px;
+              margin: auto 10px;
+              color: $icon;
+              padding-top: 5px;
             }
-        }
 
-        
-        
-        
+            i:hover {
+              transform: scale(1.1);
+            }
+          }
+
+          .router-link-active {
+            box-shadow: 1px 1px 5px rgba(50, 50, 50, 0);
+
+            i {
+              color: $active-link-icon;
+              transform: scale(1.2);
+              transition: all ease 0.1s;
+              padding-top: 4px;
+            }
+          }
+        }
+      }
     }
-    div a{
-        text-decoration: none;
-        h2{
+    div a {
+      text-decoration: none;
+      h2 {
         margin: 15px 15px;
         font-weight: 300;
-        }
+      }
     }
-}
+  }
 </style>
